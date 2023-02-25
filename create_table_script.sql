2ff687acdd6552d262a2a61b3db8617f4def9a65
ElPountie <khalis.rabehi@etu.univ-lyon1.fr>
2023-02-25 14:16:19 +0100

    database

---
+++ create_table_script.sql
@@ -0,0 +1,21 @@
+Use sakila; 
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+id int auto_increment,
+name varchar(50) NOT NULL,
+password varchar(255) NOT NULL,
+CONSTRAINT PK_user PRIMARY KEY (id)
+);
+
+CREATE TABLE tickets (
+id INT auto_increment,
+user_id INT NOT NULL default 0,
+title varchar(255) NOT NULL default '',
+description varchar(255) NOT NULL default '',
+level int(1) NOT NULL default 0,
+done integer NOT NULL default 0,
+CONSTRAINT PK_tickets PRIMARY KEY (id)
+);
+
